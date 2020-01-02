8c9f571a0b39c40e20579c72d650fd31e5a19254
Robert Truong <rticecream36@gmail.com>
2020-01-02 15:50:43 -0800

    Added River

--- board.js
+++ board.js
@@ -1,10 +1,20 @@
 const board = document.getElementById("board");
 
+const river = Math.floor(Math.random() * 7);
+
+// Creates new images
+const newImage = (source) => {
+    const image = document.createElement("img");
+    image.className = "tile-image";
+    image.src = source;
+    return image;
+}
+
 for (let i = 0; i < 7; i++)
 {
     // This creates the rows
     const row = document.createElement("div");
-    row.className="row"
+    row.className = "row";
     board.appendChild(row);
 
     // This creates tiles in the board
@@ -13,5 +23,10 @@ for (let i = 0; i < 7; i++)
         const tile = document.createElement("div");
         tile.className = "tile";
         row.appendChild(tile);
+        if (j === river)
+        {
+            const riverImage = newImage("assets/river.png");
+            tile.appendChild(riverImage);
+        }
     }
 }
